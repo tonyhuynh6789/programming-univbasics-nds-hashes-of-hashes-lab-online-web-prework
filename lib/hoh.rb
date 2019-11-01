5bd7a3a5965c89a5f04d579770f7e93df8536bc1
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-01 21:54:45 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -45,8 +45,8 @@ def naming_system
              :label => "Genus",
              :sub_category => {
                :label => "Species",
-               :sub_category =>{
-                 :label => nil,
+               :sub_category => nil
+                 
                  
                }
              }
